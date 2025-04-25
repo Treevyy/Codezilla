702b7c8afe6b922e4477848e068d5b1f28dcaaa8
Shawna Chirillo <schirillo711@gmail.com>
2025-04-25 13:24:55 -0500

    initial frontend code

---
+++ client/src/components/screens/Login.tsx
@@ -0,0 +1,32 @@
+// src/components/Login.tsx
+import React from 'react';
+import '../styles/codezilla.css';
+import { useNavigate } from 'react-router-dom';
+
+export const Login: React.FC = () => {
+  const navigate = useNavigate();
+
+  const handleLogin = () => {
+    navigate('/map');
+  };
+
+  return (
+    <div className="login-wrapper">
+      <img
+        src="/clients/backgrounds/codezilla_bkgd.png"
+        alt="rainy cityscape"
+        className="background-image"
+      />
+
+      <div className="login-content">
+        <div className="question-box">
+          <h1>Login</h1>
+          <input type="text" placeholder="username" className="login-input" />
+          <input type="password" placeholder="password" className="login-input" />
+          <p>Not a player yet?</p>
+          <button className="signup-button" onClick={handleLogin}>Login In!</button>
+        </div>
+      </div>
+    </div>
+  );
+};
