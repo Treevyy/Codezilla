0f42f0ec4f820330a8d22c4b48fd45f452d441da
hazyplebian <michael.mangieri@yahoo.com>
2025-04-26 13:29:41 -0400

    added game over and victory pages, along with corresponding stylings

--- client/src/components/screens/GameOver.tsx
+++ client/src/components/screens/GameOver.tsx
@@ -1,11 +1,48 @@
-// GameOver you lost screen :(
-// TODO: IMPORT LIBRARIES: REACT, REACT-ROUTER-DOM
-// TODO: FUNCTIONAL GAMEOVER COMPONENT
-// TODO: USENAVIGATE()
-// TODO: ADD IMG SRC "/clients/backgrounds/codezilla_bkgd.png"
-// TODO: NAVIGATE USERS BACK TO THE MAP
-// TODO: CREATE A CONTAINER GAMEOVER-CONTAINER W/ A GAME OVER HEADING
-// TODO: CREATE A GAME OVER MESSAGE
-// TODO: CREATE A RESTRT HANDLE
-// TODO: EXPORT DEFAULT GAMEOVER;
+import './codezilla.css';
 
+const GameOverPage = ({
+  backgroundUrl = '/assets/background.jpg',
+  logoUrl       = '/assets/codezilla-logo.png',
+  avatarUrl     = '/assets/player-avatar.png',
+  codezillaUrl  = '/assets/codezilla.png',
+}) => {
+  return (
+    <div
+      className="game-over-page"
+      style={{ backgroundImage: `url(${backgroundUrl})` }}
+    >
+      <img
+        className="game-over-logo"
+        src={logoUrl}
+        alt="Codezilla Logo"
+      />
+
+      <div className="game-over-container">
+        <h1 className="game-over-title">Game Over!</h1>
+
+        <div className="game-over-images">
+          <img
+            className="player-avatar"
+            src={avatarUrl}
+            alt="Player Avatar"
+          />
+          <img
+            className="codezilla-avatar"
+            src={codezillaUrl}
+            alt="Codezilla"
+          />
+        </div>
+
+        <h2 className="game-over-subtitle">
+          You were defeated by Codezilla!
+        </h2>
+
+        <p className="game-over-cta">
+          Try again to become a code master
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default GameOverPage;
