d92cac302f5da460c2ff61b25a0e70d822db0046
Jacquilyn F <jacquilyn.fletcher89@gmail.com>
2025-04-30 19:31:30 -0500

    edits to jwt

--- server/src/models/User.ts
+++ server/src/models/User.ts
@@ -1,66 +1,40 @@
-import { Schema, model, Document } from 'mongoose';
+import mongoose, { Document, Schema } from 'mongoose';
 import bcrypt from 'bcrypt';
 
-// Define an interface for the User document
-interface IUser extends Document {
-    username: string;
-    email: string;
-    password: string;
-    correctAnswers: number;
-    wrongAnswers: number;
-    isCorrectPassword(password: string): Promise<boolean>;
+export interface IUser extends Document {
+  username: string;
+  email: string;
+  password: string;
+  correctAnswers: number;
+  wrongAnswers: number;
+  score: number;
+  isCorrectPassword(password: string): Promise<boolean>;
 }
 
-// Define the schema for the User document
-const userSchema = new Schema<IUser>(
-    {
-        username: {
-            type: String,
-            required: true,
-            unique: true,
-            trim: true,
-        },
-        email: {
-            type: String,
-            required: true,
-            unique: true,
-            match: [/.+@.+\..+/, 'Must match an email address!'],
-        },
-        password: {
-            type: String,
-            required: true,
-            minlength: 5,
-        },
-        correctAnswers: {
-            type: Number,
-            default: 0,
-        },
-        wrongAnswers: {
-            type: Number,
-            default: 0,
-        },
-    },
-    {
-        timestamps: true,
-        toJSON: { getters: true },
-        toObject: { getters: true },
-    }
-);
-
-userSchema.pre<IUser>('save', async function (next) {
-    if (this.isNew || this.isModified('password')) {
-        const saltRounds = 10;
-        this.password = await bcrypt.hash(this.password, saltRounds);
-    }
-
-    next();
+const UserSchema: Schema = new Schema({
+  username: { type: String, required: true },
+  email: { type: String, required: true, unique: true },
+  password: { type: String, required: true },
+  correctAnswers: { type: Number, default: 0 },
+  wrongAnswers: { type: Number, default: 0 },
+  score: { type: Number, default: 0 },
 });
 
-userSchema.methods.isCorrectPassword = async function (password: string): Promise<boolean> {
-    return bcrypt.compare(password, this.password);
+// Pre-save hook to hash the password
+UserSchema.pre<IUser>('save', async function (next) {
+  if (!this.isModified('password')) return next();
+  try {
+    const salt = await bcrypt.genSalt(10);
+    this.password = await bcrypt.hash(this.password, salt);
+    next();
+  } catch (err) {
+    next(err as mongoose.CallbackError);
+  }
+});
+
+// Method to compare passwords
+UserSchema.methods.isCorrectPassword = async function (password: string): Promise<boolean> {
+  return bcrypt.compare(password, this.password);
 };
 
-const User = model<IUser>('User', userSchema);
-
-export default User;
-
+export default mongoose.model<IUser>('User', UserSchema);
