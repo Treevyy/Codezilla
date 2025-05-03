57cee8481e4ea03107103e96e335286cff0d6fa4
Treevyy <aarontrevormoore@gmail.com>
2025-05-02 23:17:32 -0500

    getting rid of chuncky error

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -10,7 +10,8 @@ export default defineConfig({
     },
   },
   build: {
-    outDir: 'dist', // Ensures the output is in client/dist
+    outDir: 'dist',          // Ensures the output is in client/dist
+    chunkSizeWarningLimit: 1024, // Warn only if a chunk exceeds 1 MiB (1024 KiB)
   },
   server: {
     port: 3000,
@@ -29,3 +30,36 @@ export default defineConfig({
     },
   },
 });
+
+
+// import path from 'path';
+// import { defineConfig } from 'vite';
+// import react from '@vitejs/plugin-react';
+
+// export default defineConfig({
+//   plugins: [react()],
+//   resolve: {
+//     alias: {
+//       '@': path.resolve(__dirname, 'src'),
+//     },
+//   },
+//   build: {
+//     outDir: 'dist', // Ensures the output is in client/dist
+//   },
+//   server: {
+//     port: 3000,
+//     open: true,
+//     proxy: {
+//       '/graphql': {
+//         target: 'http://localhost:3001/',
+//         changeOrigin: true,
+//         secure: false,
+//       },
+//       '/api': {
+//         target: 'http://localhost:3001/',
+//         changeOrigin: true,
+//         secure: false,
+//       },
+//     },
+//   },
+// });
