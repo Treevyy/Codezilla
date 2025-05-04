55df9e1abebdfef395d1adb2eb0aac15965b5c33
Carmen Wheeler <carmenwh33l3r@gmail.com>
2025-05-04 16:33:15 -0500

    updated backgroundmusic file to fix music overlapping

--- client/src/components/screens/Questions.tsx
+++ client/src/components/screens/Questions.tsx
@@ -236,23 +236,27 @@ const Questions: React.FC = () => {
               ))}
 
             <div className="mb-4 text-white text-base text-left whitespace-pre-wrap">
-              <ReactMarkdown
-                components={{
-                  code({ inline, children, ...props }: any) {
-                    return inline ? (
-                      <code className="inline-code" {...props}>
-                        {children}
-                      </code>
-                    ) : (
-                      <div className="bg-gray-800 p-4 rounded-md text-sm font-mono shadow-lg overflow-x-auto">
-                        <code {...props}>{children}</code>
-                      </div>
-                    );
-                  },
-                }}
-              >
-                {question.question}
-              </ReactMarkdown>
+  <ReactMarkdown
+  components={{
+    p({ node, children, ...props }) {
+      return <div className="mb-2" {...props}>{children}</div>;
+    },
+    code({ inline, children, ...props }: { inline?: boolean; children?: React.ReactNode }) {
+      return inline ? (
+        <code className="inline-code" {...props}>
+          {children}
+        </code>
+      ) : (
+        <pre className="bg-gray-800 p-4 rounded-md text-sm font-mono shadow-lg overflow-x-auto">
+          <code {...props}>{children}</code>
+        </pre>
+      );
+    },
+  }}
+>
+  {question.question}
+</ReactMarkdown>
+
             </div>
 
             <div className="text-left flex flex-col items-start gap-2 text-white">
